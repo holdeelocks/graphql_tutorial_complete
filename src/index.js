66c1d80e71f672937c2e7111ab476ff4727b32fd
Holden B <holden.bucher.92@gmail.com>
2019-01-18 14:58:14 -0600

    backend for hackernews GraphQL app tutorial

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const { GraphQLServer } = require("graphql-yoga");
+const { prisma } = require("./generated/prisma-client");
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
+const Subscription = require("./resolvers/Subscription");
+const Vote = require("./resolvers/Vote");
+
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  }
+];
+
+let idCount = links.length;
+
+const resolvers = {
+  Query,
+  Mutation,
+  Subscription,
+  User,
+  Link,
+  Vote
+};
+
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers,
+  context: request => {
+    return {
+      ...request,
+      prisma
+    };
+  }
+});
+server.start(() => console.log(`Server is up and running on localhost:4000`));
---
+++ src/resolvers/Query.js
@@ -0,0 +1,41 @@
+async function feed(parent, args, context, info) {
+  const where = args.filter
+    ? {
+        OR: [{ description_contains: args.filter }, { url_contains: args.filter }]
+      }
+    : {};
+
+  const links = await context.prisma.links({
+    where,
+    skip: args.skip,
+    first: args.first,
+    orderBy: args.orderBy
+  });
+
+  const count = await context.prisma
+    .linksConnection({
+      where
+    })
+    .aggregate()
+    .count();
+
+  return {
+    links,
+    count
+  };
+}
+
+function info(parents, args, context, info) {
+  return "This is the API of a Hackernews Clone";
+}
+
+function findById(parents, args, context, info) {
+  let links = context.prisma.links;
+  return links.find(link => link.id === args.id);
+}
+
+module.exports = {
+  feed,
+  info,
+  findById
+};
